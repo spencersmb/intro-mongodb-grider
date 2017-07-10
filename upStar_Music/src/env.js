4272e8293a46e1e950856f8d436e260ad8eef6da
spencersmb <spencer.bigum@gmail.com>
2017-07-09 23:20:11 -0400

    try3

---
+++ upStar_Music/src/env.js
@@ -0,0 +1,7 @@
+exports.variables = {
+  USER: 'everytuesday',
+  PW: 'westwood23',
+  URI: 'ds153652.mlab.com',
+  PORT: 53652,
+  MONGODB_URI: 'mongodb://everytuesday:westwood23@ds153652.mlab.com:53652/upstarmusicapp'
+}
---
+++ upStar_Music/src/main.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import ReduxThunk from 'redux-thunk'
+import { Db, Server } from 'mongodb'
+import reducers from './reducers'
+import Routes from './router'
+import mongoose from 'mongoose'
+import './seeds'
+// require('./config.json')
+import { variables } from './env'
+
+mongoose.Promise = Promise
+
+const App = () => {
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  )
+}
+
+// const db = new Db('upstar_music', new Server('localhost', 27017))
+const db = new Db('upstarmusicapp', new Server(variables.URI, variables.PORT
+))
+
+db.open((err, dba) => {
+  dba.authenticate(variables.USER, variables.pw, (err, success) => {
+    if (success) {
+      // callback(null, db);
+      window.db = db
+      mongoose.connect(variables.MONGODB_URI, {
+        useMongoClient: true
+        /* other options */
+      })
+      mongoose.connection
+        .once('open', () => {
+          ReactDOM.render(<App />, document.getElementById('root'))
+        })
+        .on('error', (error) => {
+          console.warn('Warning', error)
+        })
+    } else {
+      // callback(err ? err : new Error('Could not authenticate user ' + user), null);
+    }
+  })
+})
+// .then(() => {
+//   window.db = db
+//   // mongoose.connect('mongodb://localhost/upstar_music')
+//   mongoose.connect(variables.MONGODB_URI, {
+//     useMongoClient: true
+//     /* other options */
+//   })
+//   mongoose.connection
+//     .once('open', () => {
+//       ReactDOM.render(<App />, document.getElementById('root'))
+//     })
+//     .on('error', (error) => {
+//       console.warn('Warning', error)
+//     })
+// })
