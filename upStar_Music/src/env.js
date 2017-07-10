a1f243a40d7fdf5134c9436e60e2738ab2ebd037
spencer <spencer.bigum@gmail.com>
2017-07-10 12:19:50 -0400

    repo restart

--- upStar_Music/src/env.js
+++
@@ -1,7 +0,0 @@
-exports.variables = {
-  USER: 'everytuesday',
-  PW: 'westwood23',
-  URI: 'ds153652.mlab.com',
-  PORT: 53652,
-  MONGODB_URI: 'mongodb://everytuesday:westwood23@ds153652.mlab.com:53652/upstarmusicapp'
-}
